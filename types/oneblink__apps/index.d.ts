482ba5abe1dcd7f04ba54ca174f6836057dd8ffc
Zac Turner <zac@oneblink.io>
2020-07-28 17:06:20 +1000

    ON-7127 # Added authService types

--- types/oneblink__apps/index.d.ts
+++ types/oneblink__apps/index.d.ts
@@ -4,7 +4,53 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.9
 
+interface OneBlinkAppsTenant {
+    apiOrigin: string;
+    loginDomain: string;
+}
+type NoV = null | void;
+interface UserProfile {
+    isSAMLUser: boolean;
+    providerType: string;
+    providerUserId: string;
+    userId: string;
+    username: string;
+    email: string | NoV;
+    firstName: string | NoV;
+    lastName: string | NoV;
+    fullName: string | NoV;
+    picture: string | NoV;
+    role: string | NoV;
+    supervisor:
+        | {
+              fullName: string | NoV;
+              email: string | NoV;
+              providerUserId: string | NoV;
+          }
+        | NoV;
+}
 declare namespace offlineService {
     function isOffline(): boolean;
 }
-export { offlineService };
+
+declare namespace authService {
+    function init(
+        tenant: OneBlinkAppsTenant,
+        options: {
+            oAuthClientId: string;
+            useSAML: boolean;
+        },
+    ): void;
+    function getUserFriendlyName(): string | null;
+    function getIssuerFromJWT(jwt: string | null | void): string | void;
+    function isAuthorised(formsAppId: number): Promise<boolean>;
+    function requestAccess(formsAppId: number): Promise<void>;
+    function login(): Promise<string>;
+    function handleAuthentication(): Promise<string>;
+    function isLoggedIn(): boolean;
+    function getIdToken(): Promise<string | void>;
+    function getUserProfile(): UserProfile | null;
+    function logout(): Promise<void>;
+}
+
+export { offlineService, authService };
